7197838bced7e25051e0ebcea5cda50cbe73da47
Wesley Robb <wesley.robb@derivco.co.za>
2023-10-27 16:01:34 +0200

    Add focus plugin

--- init.lua
+++ init.lua
@@ -601,5 +601,8 @@ require("oil").setup({
   },
 })
 
+-- [[ Configure focus ]]
+require('focus').setup()
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
