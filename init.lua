a6372c5967c090248d274a5d9eff2851737a0ad7
Wesley Robb <wesley.robb@derivco.co.za>
2023-10-27 09:39:08 +0200

    Fix oil not showing hidden files

--- init.lua
+++ init.lua
@@ -585,5 +585,21 @@ cmp.setup {
 -- [[ Configure mini.align ]]
 require('mini.align').setup()
 
+-- [[ Configure oil.nvim ]]
+require("oil").setup({
+  view_options = {
+    -- Show files and directories that start with "."
+    show_hidden = true,
+    -- This function defines what is considered a "hidden" file
+    is_hidden_file = function(name, bufnr)
+      return vim.startswith(name, ".")
+    end,
+    -- This function defines what will never be shown, even when `show_hidden` is set
+    is_always_hidden = function(name, bufnr)
+      return vim.startswith(name, ".git")
+    end,
+  },
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
