fa70be00553704c9ee852a7ef040272e6500ae9f
Wesley Robb <wesley.robb@derivco.co.za>
2024-01-25 11:07:34 +0200

    Fix oil setup not running

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -8,27 +8,19 @@ return {
   },
   cmd = "Oil",
   init = function()
-    local setup = function()
       require("oil").setup({
         view_options = {
           -- Show files and directories that start with "."
           show_hidden = true,
           -- This function defines what is considered a "hidden" file
-          is_hidden_file = function(name, bufnr)
+          is_hidden_file = function(name, _)
             return vim.startswith(name, ".")
           end,
           -- This function defines what will never be shown, even when `show_hidden` is set
-          is_always_hidden = function(name, bufnr)
+          is_always_hidden = function(name, _)
             return vim.startswith(name, ".git")
           end,
         },
       })
-    end
-    if vim.fn.argc(-1) == 1 then
-      local stat = vim.loop.fs_stat(vim.fn.argv(0))
-      if stat and stat.type == "directory" then
-        setup()
-      end
-    end
   end,
 }
