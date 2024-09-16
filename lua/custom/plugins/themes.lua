7c5286ce44d431265dd21c0d6882e82cd11c997f
Wesley Robb <wesley.robb@derivco.co.za>
2024-09-16 10:22:43 +0200

    Add markdown rendering and change colorscheme

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -1,7 +1,10 @@
 return {
-  "Shatur/neovim-ayu",
-  priority = 1000,
+  'projekt0n/github-nvim-theme',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.cmd.colorscheme 'ayu-dark'
+    require('github-theme').setup()
+
+    vim.cmd('colorscheme github_light_high_contrast')
   end,
 }
