2f135d6a75e80946afef951f9d2ac493c7452934
Wesley Robb <wesley.robb@derivco.co.za>
2024-09-16 13:43:24 +0200

    Change to github dark theme

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -5,6 +5,6 @@ return {
   config = function()
     require('github-theme').setup()
 
-    vim.cmd('colorscheme github_light_high_contrast')
+    vim.cmd('colorscheme github_dark_high_contrast')
   end,
 }
