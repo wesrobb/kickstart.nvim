c5bc106a13fdbd15696567afe999a3a40b5417a7
Wesley Robb <wesrobb86@gmail.com>
2024-09-19 19:57:14 +0200

    Remove more resession config

--- init.lua
+++ init.lua
@@ -680,20 +680,6 @@ vim.keymap.set("n", "<leader>rbf", "<CMD>SearchReplaceMultiBufferCFile<CR>", opt
 -- show the effects of a search / replace in a live preview window
 vim.o.inccommand = "split"
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    -- Only load the session if nvim was started with no args
-    if vim.fn.argc(-1) == 0 then
-    -- Save these to a different directory, so our manual sessions don't get polluted
-      resession.load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
-    end
-  end,
-})
-vim.api.nvim_create_autocmd("VimLeavePre", {
-  callback = function()
-    resession.save(vim.fn.getcwd(), { dir = "dirsession", notify = false })
-  end,
-})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
