df644d5c1680d3e09c7ed4b30b68492e7ee7f77a
Wesley Robb <wesley.robb@derivco.co.za>
2024-10-02 13:21:30 +0200

    Only setup powershell if on windows

--- init.lua
+++ init.lua
@@ -348,24 +348,26 @@ vim.o.termguicolors = true
 vim.o.laststatus = 3 -- Required from avante.nvim
 
 -- Check if 'pwsh' is executable and set the shell accordingly
-if vim.fn.executable('pwsh') == 1 then
+if jit.os == "Windows" then
+  if vim.fn.executable('pwsh') == 1 then
     vim.o.shell = 'pwsh'
-else
+  else
     vim.o.shell = 'powershell'
+  end
+
+  -- Setting shell command flags
+  vim.o.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[\'Out-File:Encoding\']=\'utf8\';'
+
+  -- Setting shell redirection
+  vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+  -- Setting shell pipe
+  vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+  -- Setting shell quote options
+  vim.o.shellquote = ''
+  vim.o.shellxquote = ''
 end
-
--- Setting shell command flags
-vim.o.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[\'Out-File:Encoding\']=\'utf8\';'
-
--- Setting shell redirection
-vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
-
--- Setting shell pipe
-vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
-
--- Setting shell quote options
-vim.o.shellquote = ''
-vim.o.shellxquote = ''
 
 -- Font settings
 -- https://www.nerdfonts.com/font-downloads
