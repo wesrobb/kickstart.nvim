ea2a5e22fd8704a7e540b423e70e6b017e9da32e
Wesley Robb <wesrobb86@gmail.com>
2024-10-22 20:49:35 +0200

    Set makeprg to build.ps1 on windows

--- init.lua
+++ init.lua
@@ -340,9 +340,11 @@ if jit.os == "Windows" then
 
   vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
   vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-  vim.o.shellpipe = '2>&1 | %${ "$_" } | tee %s; exit $LastExitCode'
+  vim.o.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
   vim.o.shellquote = ''
   vim.o.shellxquote = ''
+
+  vim.o.makeprg = "./build.ps1"
 end
 
 -- Font settings
