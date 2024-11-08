8e494a791d3702d90e051e224c8e80a1bc404d92
Wesley Robb <wesley.robb@derivco.co.za>
2024-11-08 10:49:45 +0200

    Make toggleterm window float

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -3,6 +3,16 @@ return {
   config = function()
     require('toggleterm').setup {
       open_mapping = [[<c-t>]],
+      direction = "float",
+      float_opts = {
+        border = "curved",
+        width = function (term)
+            return math.floor(vim.o.columns * 0.95)
+        end,
+        height = function (term)
+            return math.floor(vim.o.lines * 0.9)
+        end
+      },
     }
   end,
   cmd = "ToggleTerm",
